b38423dd055071209db39277e15ec9e04852584b
Rafael Watanabe <rafawat0428@gmail.com>
2023-02-09 14:35:53 +0900

    Stage Generator and Sky

--- Assets/Scripts/NejikoController.cs
+++ Assets/Scripts/NejikoController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class NejikoController : MonoBehaviour
 {
+    const int MinLane = -2;
+    const int MaxLane = 2;
+    const float LaneWidth = 1.0f;
 
     CharacterController controller;
     Animator animator;
 
     Vector3 moveDirection = Vector3.zero;
+    int targetLane;
 
     public float gravity;
     public float speedZ;
+    public float speedX;
     public float speedJump;
+    public float accelerationZ;
     
     // Start is called before the first frame update
     void Start()
@@ -24,6 +30,19 @@ public class NejikoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Debbug
+        if(Input.GetKeyDown("left")) MoveToLeft();
+        if(Input.GetKeyDown("right")) MoveToRight();
+        if(Input.GetKeyDown("space")) Jump();
+
+        //Z方向に常に前進させる
+        float acceleratedZ = moveDirection.z + (accelerationZ * Time.deltaTime);
+        moveDirection.z = Mathf.Clamp(acceleratedZ,0,speedZ);
+
+        //X方向の目標のポジションまでの差分の割合を計算
+        float ratioX = (targetLane * LaneWidth - transform.position.x)/LaneWidth;
+        moveDirection.x = ratioX * speedX;
+
         if(controller.isGrounded)
         {
             if(Input.GetAxis("Vertical")>0.0f)
@@ -52,5 +71,25 @@ public class NejikoController : MonoBehaviour
         if(controller.isGrounded) moveDirection.y = 0;
 
         animator.SetBool("run", moveDirection.z > 0.0f);
+    }
+
+    public void MoveToLeft()
+    {
+        if (controller.isGrounded && targetLane > MinLane) targetLane--;
+    }
+
+    public void MoveToRight()
+    {
+        if (controller.isGrounded && targetLane < MaxLane) targetLane++;
+    }
+
+    public void Jump()
+    {
+        if(controller.isGrounded)
+        {
+            moveDirection.y = speedJump;
+
+            animator.SetTrigger("jump");
+        }
     }
 }
---
+++ Assets/Scripts/StageGenerator.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageGenerator : MonoBehaviour
+{
+
+    const int StageChipSize = 30;
+
+    int currentChipIndex;
+
+    public Transform character;
+    public GameObject[] stageChips;
+    public int startChipIndex;
+    public int preInstantiate;
+    public List<GameObject> generatedStageList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentChipIndex = startChipIndex -1;
+        UpdateStage(preInstantiate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int charaPositionIdex = (int)(character.position.z/StageChipSize);
+
+        if(charaPositionIdex + preInstantiate > currentChipIndex)
+        {
+            UpdateStage(charaPositionIdex + preInstantiate);
+        }
+    }
+
+    void UpdateStage(int toChipIndex)
+    {
+        if(toChipIndex <= currentChipIndex) return;
+
+        for(int i = currentChipIndex + 1; i<= toChipIndex;i++)
+        {
+            GameObject stageObject = GenerateStage(i);
+
+            generatedStageList.Add(stageObject);
+        }
+
+        while(generatedStageList.Count > preInstantiate + 2)DestroyOldestStage();
+        currentChipIndex=toChipIndex;
+    }
+
+    GameObject GenerateStage(int chipIndex)
+    {
+        int nextStageChip = Random.Range(0, stageChips.Length);
+
+        GameObject stageObject = (GameObject)Instantiate(
+            stageChips[nextStageChip],
+            new Vector3(0,0, chipIndex * StageChipSize),
+            Quaternion.identity
+        );
+
+        return stageObject;
+    }
+
+    void DestroyOldestStage()
+    {
+        GameObject oldStage = generatedStageList[0];
+        generatedStageList.RemoveAt(0);
+        Destroy(oldStage);
+    }
+}
